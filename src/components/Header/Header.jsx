00074546c85e225ed2ba67003c75c46a3bd66ff7
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-20 19:30:08 +0100

    hero mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,14 @@ import "./header.css";
 const Header = () => {
   return (
     <section id="header" className="container header header__container">
-      <h2 className="text-light">Hello, I'm Joseph</h2>
-      <h1>
-        I enjoy <span className="span">building</span> and{" "}
-        <span className="span">designing</span> for the web.
-      </h1>
-      <button className="btn btn-primary ">Tell me more</button>
+      <div>
+        <h2>Hello, I'm Joseph</h2>
+        <h1>
+          I enjoy <span className="span">building</span> and{" "}
+          <span className="span">designing</span> for the web.
+        </h1>
+        <button className="btn btn-primary ">Tell me more</button>
+      </div>
     </section>
   );
 };
