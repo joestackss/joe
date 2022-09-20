fa58fc37dc12abcbf07dfbe3b210a8e9dfa79374
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-20 19:03:02 +0100

    project mobile

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,53 +1,56 @@
 import React from "react";
-import { FaTwitter, FaSkype, FaVimeoV } from "react-icons/fa";
+import { BiLinkExternal } from "react-icons/bi";
+import { BsGithub } from "react-icons/bs";
 import "./project.css";
 
 const Project = () => {
   return (
     <section className="container project project__container">
-      <h2>Here are a few of my favorite projects.</h2>
-      <div className="flex flex-between ">
+      <div className="flex flex-between">
+        <h2>Here are a few of my favorite projects.</h2>
+        <div className="line-div"></div>
+      </div>
+
+      <div className=" card-main flex flex-between ">
         <div className="card-col">
-          <img src="yei-learn.png" alt="" />
+          <img src="yei-learn.png" alt="" className="img-div" />
           <div className="flex flex-between">
             <h3>Learn.TheYEI</h3>
             <div>
-              <FaTwitter className="icons" />
-              <FaSkype className="icons" />
+              <BiLinkExternal className="icons" />
+              <BsGithub className="icons" />
             </div>
           </div>
           <p>All the resources you will need to get a boost into economics.</p>
           <div className="flex flex-wrap">
             <p className="btn-tag ">React</p>
-            <p className="btn-tag">JSON</p>
             <p className="btn-tag">Javascript</p>
             <p className="btn-tag">CSS</p>
           </div>
         </div>
         <div className="card-col">
-          <img src="yei-learn.png" alt="" />
+          <img src="buildfaster.png" alt="" className="img-div" />
           <div className="flex flex-between">
             <h3>BuildFaster</h3>
             <div>
-              <FaTwitter className="icons" />
-              <FaSkype className="icons" />
+              <BiLinkExternal className="icons" />
+              <BsGithub className="icons" />
             </div>
           </div>
           <p>Beautiful HTML templates ready-made for your next landing page.</p>
           <div className="flex flex-wrap">
             <p className="btn-tag">React</p>
-            <p className="btn-tag">JSON</p>
             <p className="btn-tag">Javascript</p>
             <p className="btn-tag">CSS</p>
           </div>
         </div>
         <div className="card-col">
-          <img src="yei-learn.png" alt="" />
+          <img src="react-emoji-search.png" alt="" className="img-div" />
           <div className="flex flex-between">
             <h3>React Emoji Search</h3>
             <div>
-              <FaTwitter className="icons" />
-              <FaSkype className="icons" />
+              <BiLinkExternal className="icons" />
+              <BsGithub className="icons" />
             </div>
           </div>
           <p>
@@ -56,7 +59,6 @@ const Project = () => {
           </p>
           <div className="flex flex-wrap">
             <p className="btn-tag">React</p>
-            <p className="btn-tag">JSON</p>
             <p className="btn-tag">Javascript</p>
             <p className="btn-tag">CSS</p>
           </div>
