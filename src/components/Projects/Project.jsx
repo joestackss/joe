f8a108a8116126ea3b810d1b5959a429e2971ce3
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-21 10:26:33 +0100

    responsiveness

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -11,10 +11,65 @@ const Project = () => {
         <div className="line-div"></div>
       </div>
 
-      <div className=" card-main flex flex-between ">
+      <div className=" card-main flex flex-between margin-bottom-lg ">
         <div className="card-col">
           <img src="yei-learn.png" alt="" className="img-div" />
           <div className="flex flex-between">
+            <h3>Learn.TheYEI</h3>
+            <div>
+              <BiLinkExternal className="icons" />
+              <BsGithub className="icons" />
+            </div>
+          </div>
+          <p className="paragraph">All the resources you will need to get a boost into economics.</p>
+          <div className="flex flex-wrap">
+            <p className="btn-tag ">React</p>
+            <p className="btn-tag">Javascript</p>
+            <p className="btn-tag">CSS</p>
+          </div>
+        </div>
+        <div className="card-col">
+          <img src="buildfaster.png" alt="" className="img-div" />
+          <div className="flex flex-between">
+            <h3>BuildFaster</h3>
+            <div>
+              <BiLinkExternal className="icons" />
+              <BsGithub className="icons" />
+            </div>
+          </div>
+          <p className="paragraph">Beautiful HTML templates ready-made for your next landing page.</p>
+          <div className="flex flex-wrap">
+            <p className="btn-tag">React</p>
+            <p className="btn-tag">Javascript</p>
+            <p className="btn-tag">CSS</p>
+          </div>
+        </div> 
+        <div className="card-col">
+          <img src="react-emoji-search.png" alt="" className="img-div" />
+          <div className="flex flex-between">
+            <h3>React Emoji </h3>
+            <div>
+              <BiLinkExternal className="icons" />
+              <BsGithub className="icons" />
+            </div>
+          </div>
+          <p className="paragraph">
+            1800+ emojis at your fingertips. Ready-to-use with just copy/paste
+          </p>
+          <div className="flex flex-wrap">
+            <p className="btn-tag">React</p>
+            <p className="btn-tag">Javascript</p>
+            <p className="btn-tag">CSS</p>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className=" card-main flex flex-between ">
+        <div className="card-col ">
+          <img src="yei-learn.png" alt="" className="img-div" />
+          <div className="flex flex-between ">
             <h3>Learn.TheYEI</h3>
             <div>
               <BiLinkExternal className="icons" />
