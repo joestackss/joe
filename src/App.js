f528d5411a7a4f8ce9f2d8f4bbf3ef88304e0936
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-22 14:57:59 +0100

    after updating react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
@@ -6,6 +7,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Project from "./components/Projects/Project";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Tools from "./components/Tools/Tools";
+import { Browser as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
