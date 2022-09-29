b483001bc4a5aced4b990a879e7bfdb6d42708f4
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-29 11:02:11 +0100

    testimonial

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,41 +3,70 @@ import "./testimonials.css";
 
 const Testimonials = () => {
   return (
-    <section id="testimonial" className="testimonial container testimonial__container">
-      <div className="flex flex-between" data-aos="fade-up" data-aos-duration="1000">
+    <section
+      id="testimonial"
+      className="testimonial container testimonial__container"
+    >
+      <div
+        className="flex flex-between"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      >
         <h2>Why people love my work!</h2>
       </div>
 
       <div className=" card-main flex flex-between margin-bottom-lg ">
         <div className="card-column flex-between ">
-          <div className="card-col margin-bottom-lg" data-aos="fade-up" data-aos-duration="1000">
+          <div
+            className="card-col margin-bottom-lg"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
             <h3>
-              "The communication throughout the project was spot on and the
-              genre of design was carried out well."
+              "Joseph is so easy to work with. He listened to what I had
+              envisioned and brought it to life. I can’t recommend him enough.
+              I’ll definitely be using him for all my needs."
             </h3>
             <p className="paragraph">
-              Wilson Bright{" "}
-              <span className="spans">- Co-Founder of BlockSurvey</span>{" "}
+              Beatriz Alberti
+              <span className="spans">- Co-Founder of Alberti Studio</span>
             </p>
           </div>
-          <div className="card-col margin-bottom-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="300">
+          <div
+            className="card-col margin-bottom-lg"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-offset="300"
+          >
             <h3>
-              "The code was clean, easy to understand, and exactly what I was
-              looking for.""
+              "Joseph is my go-to freelancer. I've been working with him for
+              months already and looking forward to continuing our efforts. He
+              knows exactly what to do and he does iit well."
             </h3>
             <p className="paragraph">
-              Philip Imperato{" "}
-              <span className="spans"> - CEO of VisualBonus</span>
+              Fay Goldstein
+              <span className="spans">
+                {" "}
+                - Head of communication - Entree Capital
+              </span>
             </p>
           </div>
-          <div className="card-col" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="300">
+          <div
+            className="card-col"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-offset="300"
+          >
             <h3>
-              "My vision was executed perfectly. The quality and speed of the
-              work was excellent."
+              "Joseph was a pleasure to work with on my website development
+              project. He was very quick to get changes back to me, and worked
+              well in collaboration to make the website look and function as
+              desired. He is knowledgeable in website development, and
+              provides a wonderful finished product."
             </h3>
             <p className="paragraph">
-              Zach Green{" "}
-              <span className="spans"> - Founder of Hangxiety Shrink</span>
+            Uwe Janotta
+              <span className="spans"> - Ceo {"&"} Founder, Berit</span>
             </p>
           </div>
         </div>
