8344293c039fee4199e393ed82036d2551d1a98a
olamideare <63550820+olamideare@users.noreply.github.com>
2022-12-30 18:37:01 +0100

    Links Updated

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -16,39 +16,6 @@ const Project = () => {
       </div>
 
       <div className="card-main margin-bottom-lg ">
-
- <div className="card-col" data-aos="fade-up" data-aos-duration="1000">
-          <a href="https://passworth.netlify.app/" target="_blank">
-            <img src="Realty-View.png" alt="" className="img-div" />
-          </a>
-          <div>
-            <div className="flex flex-between">
-              <h3>Realty View </h3>
-              <div>
-                <a href="https://realtyview.com/" target="_blank">
-                  <BiLinkExternal className="icons" />
-                </a>
-                <a
-                  href="https://github.com/joestackss"
-                  target="_blank"
-                >
-                  <BsGithub className="icons" />
-                </a>
-              </div>
-            </div>
-            <div>
-              <p className="paragraph">
-                A SaaS app where Millions Of Property Records can be searched.
-              </p>
-              <div className="flex flex-wrap">
-                <p className="btn-tag">React</p>
-                <p className="btn-tag">Stripe</p>
-                <p className="btn-tag">Firebase</p>
-                <p className="btn-tag">Taiwind</p>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="card-col" data-aos="fade-up" data-aos-duration="1000">
           <a href="https://www.realtyview.com/" target="_blank">
             <img src="Realty-View.png" alt="" className="img-div" />
